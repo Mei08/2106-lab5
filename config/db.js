6b86cd3dc5ca0bb487180b4f4ab5a08a5e4b7b3a
Mei08 <125234mary@gmail.com>
2016-03-17 23:33:56 -0400

    models

---
+++ config/db.js
@@ -0,0 +1,12 @@
+// create public connection string
+module.exports = {
+
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    'url': 'mongodb://mei:123456@ds064278.mlab.com:64278/2106',
+    'githubClientId': 'b2a3c5d04a43c08df470',
+    'githubClientSecret':'c105b93b9903c4d17ae5fab726fc7554b417b980',
+    'githubCallbackUrl' : 'http://localhost:3000/auth/githubcallback'
+};
